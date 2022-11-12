18d92e1c9fdeb131742fd73d76b98f7eaf9a13c8
magracarta <rmfoal1996@gmail.com>
2022-11-12 23:45:44 +0900

    react 강의

--- react/basic/src/components/Profile.jsx
+++ react/basic/src/components/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Avartar from './Avartar';
 
-export default function Profile() {
+export default function Profile({image, name, title , isNew}) {
     return <div className='profile'>
-            <img className='photo' src='https://images.unsplash.com/photo-1668079078320-76dba982b397?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' 
-            alt='avarta'/>
-            <h1>James kim</h1>
-            <p>프론트엔드 개발자</p>
+            <Avartar image={image} isNew={isNew} />
+            <h1>{name}</h1>
+            <p>{title}</p>
         </div>
+
     
 }
 
