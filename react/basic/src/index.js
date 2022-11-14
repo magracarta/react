fa9e59b185bb1f4730181baa77600918c969a0df
magracarta <rmfoal1996@gmail.com>
2022-11-14 23:41:27 +0900

    마우스 무브 ~ 스프레드 연산자

--- react/basic/src/index.js
+++ react/basic/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppProducts from './AppProducts';
+import AppMentor from './AppMentor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProducts />
+    <AppMentor />
   </React.StrictMode>
 );
 
