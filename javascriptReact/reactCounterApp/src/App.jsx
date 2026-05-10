c5b6f1715115e0ad2a2ead8ae3a23aef94b0acd3
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-05-10 20:58:47 +0900

    useEffect
    mount
    update
    unmount

--- javascriptReact/reactCounterApp/src/App.jsx
+++ javascriptReact/reactCounterApp/src/App.jsx
@@ -1,10 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css'
 import Controller from './components/Controller'
 import Viewer from './components/Viewer'
+import Even from './Even';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
+  useEffect(()=>{
+    console.log("mount");
+  },[]);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(()=>{
+    if(!isMount.current){
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+// 3. 언마운트  : 죽음
+
+  // useEffect(()=>{
+  //   console.log(`count : ${count} / input: ${input}`);
+  // },[count,input]);
+  //의존성 배열
+  //dependcy array
+  //deps
 
   const onClickButton = (v)=>{
     setCount(count+v);
@@ -14,7 +40,14 @@ function App() {
     <div className='App'>
     <h1>Simple Counter</h1>
     <section>
+      <input value={input} onChange={(e)=>{
+        setInput(e.target.value);
+      }}/>
+    </section>
+    <section>
       <Viewer count={count}/>
+      {count %2 ===0?<Even/>: null}
+      
     </section>
     <section>
       <Controller onClickButton={onClickButton}/>
---
+++ javascriptReact/todoList/src/components/Editor.jsx
@@ -0,0 +1,37 @@
+import { useState, useRef } from "react";
+import "./Editor.css"
+
+const Editor = ({onCreate})=>{
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
+
+    const onChangeContent = (e)=>{
+        setContent(e.target.value);
+    }
+    const onKeyDown=(e)=>{
+        if(e.keyCode === 13){
+            onsubmit();
+        }
+    }
+
+    const onsubmit =()=>{
+        if(content === "") {
+            contentRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    }
+
+    return (
+        <div className="Editor">
+            <input 
+             ref={contentRef}
+             onKeyDown={onKeyDown}
+            value={content} onChange={onChangeContent} placeholder="새로운 Todos...."/>
+            <button onClick={onsubmit}>추가</button>
+        </div>
+    )
+}
+
+export default Editor;
