906ef154d544f3cb3ddfc2ad8a95c39266ed29e7
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-05-11 23:12:11 +0900

    useReducer

--- javascriptReact/todoList/src/App.jsx
+++ javascriptReact/todoList/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Editor from './components/Editor'
 import List from './components/List'
 import Header from './components/Header'
-import { useRef, useState } from 'react'
+import { useReducer, useRef, useState } from 'react'
 
   const mockData =[
     {
@@ -23,32 +23,53 @@ import { useRef, useState } from 'react'
     }
   ];
 
+  function reducer(state,action){
+    switch(action.type){
+      case  'CREATE':
+        return [action.data,...state];
+      case  'UPDATE':
+        return state.map(
+          (item)=>item.id === action.targetId
+        ?{...item,isDone: !item.isDone} 
+        : item 
+      );
+      case  'DELETE':
+        return state.filter((item)=> item.id !== action.targetId );
+        default:
+            return state;
+    }
+    
+  }
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer,mockData);
   const idRef = useRef(3);
+
+
   const onUpdate = (targetId)=>{
-    //todos State 의 값들 중에
-    //targeId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
-    //인수:todos배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
-    setTodos(
-      todos.map((todo)=>todo.id === targetId?{...todo,isDone: !todo.isDone} : todo )
-    );
+    dispatch({
+      type:"UPDATE",
+      targetId : targetId
+    })
   }
   const onDelete = (targetId)=>{
-    console.log(targetId);
-    setTodos(
-      todos.filter((todo)=> todo.id !== targetId )
-    )
+    dispatch({
+      type:"DELETE",
+      targetId : targetId
+    })
   }
 
   const onCreate = (content)=>{
-    const newTodo ={
-      id:idRef.current++,
-      isDone : false,
-      content : content,
-      date : new Date().getTime()
-    }
-    setTodos([newTodo,...todos]);
+    dispatch({
+        type : "CREATE",
+        data:{
+           id:idRef.current++,
+            isDone : false,
+            content : content,
+            date : new Date().getTime()
+        }
+    });
+    
   }
 
 
