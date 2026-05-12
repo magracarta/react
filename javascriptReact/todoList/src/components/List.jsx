eb24a51e53c6cc25eb1cc2f16a03d787406d2dde
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-05-12 22:32:57 +0900

    useMemo
    메모제이션

--- javascriptReact/todoList/src/components/List.jsx
+++ javascriptReact/todoList/src/components/List.jsx
@@ -13,9 +13,32 @@ const List = ({todos,onUpdate,onDelete})=>{
         }
         return todos.filter((todo)=>todo.content.toLowerCase().includes(search.toLowerCase()));
     },[search, todos]);
+
+    
+    const { totalCount,doneCount,notDoneCount} = useMemo(()=>{
+        console.log("getAnalyzedData 호출!");
+        const totalCount = todos.length;
+        const doneCount = todos.filter((todo)=>todo.isDone).length;
+
+        const notDoneCount = totalCount - doneCount;
+
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount
+        }
+    },[todos]);
+
+    //const { totalCount,doneCount,notDoneCount} = getAnalyzedData();
+
     return (
         <div className="List">
             <h4>Todo List 🌱</h4>
+            <div>
+                <div>total :{totalCount}</div>
+                <div>done :{doneCount}</div>
+                <div>not done :{notDoneCount}</div>
+            </div>
             <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요." />
             <div className="todos_wrapper">
                 {
