e7448ed1b5c3ad12fa0678bdd2871b7770e64352
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-05-17 15:50:38 +0900

    router

---
+++ javascriptReact/emotionalDiary/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home'
+import New from './pages/New'
+import Diary from './pages/Diary'
+import Notfound from './pages/Notfound'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
+import { getEmotionImage } from './util/get-emotion-images'
+import Button from './components/Button'
+import Header from './components/Header'
+
+// 1. "/" :  모든 일기를 조회하는 HOME 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+function App() {
+  const nav = useNavigate();
+
+  const onClickButton =()=>{
+    nav("/new");
+  } 
+  return (
+    <>
+    <Header 
+      title={"Header"}
+      leftChild={<Button text={"Left"}/>}
+      rightChild={<Button text={"Right"}/>}
+    />
+    <Button 
+      text={"123"} 
+      onClick={()=>{
+        console.log("123 번 버튼 클릭");
+      }}
+    />
+    <Button 
+      text={"123"} 
+      type = {"POSITIVE"}
+      onClick={()=>{
+        console.log("123 번 버튼 클릭");
+      }}
+    />
+    <Button 
+      text={"123"} 
+      type = {"NEGATIVE"}
+      onClick={()=>{
+        console.log("123 번 버튼 클릭");
+      }}
+    />
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/new' element={<New/>}/>
+        <Route path='/diary/:id' element={<Diary/>}/>
+        <Route path='*' element={<Notfound/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
